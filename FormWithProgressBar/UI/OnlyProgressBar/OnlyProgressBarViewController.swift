183ec6a87d23bd524a76002683db70b53e990de8
kz56cd <kz56cd@gmail.com>
2018-05-23 17:10:35 +0900

    Add progress bar

--- FormWithProgressBar/UI/OnlyProgressBar/OnlyProgressBarViewController.swift
+++ FormWithProgressBar/UI/OnlyProgressBar/OnlyProgressBarViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import FlexibleSteppedProgressBar
 
 final class OnlyProgressBarViewController: UIViewController {
 
+    @IBOutlet weak var progressBar: FlexibleSteppedProgressBar!
+    //    var progressBar: FlexibleSteppedProgressBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        prepareProgressBar()
     }
     
     @IBAction func closeButtonTapped(_ sender: UIButton) {
@@ -20,5 +25,35 @@ final class OnlyProgressBarViewController: UIViewController {
 }
 
 extension OnlyProgressBarViewController {
-    // stub
+    fileprivate func prepareProgressBar() {
+        progressBar.delegate = self
+    }
+}
+
+extension OnlyProgressBarViewController: FlexibleSteppedProgressBarDelegate {
+    func progressBar(_ progressBar: FlexibleSteppedProgressBar, didSelectItemAtIndex index: Int) {
+        
+    }
+    
+    func progressBar(_ progressBar: FlexibleSteppedProgressBar, willSelectItemAtIndex index: Int) {
+        
+    }
+    
+    func progressBar(_ progressBar: FlexibleSteppedProgressBar, canSelectItemAtIndex index: Int) -> Bool {
+        return true
+    }
+    
+    func progressBar(_ progressBar: FlexibleSteppedProgressBar, textAtIndex index: Int, position: FlexibleSteppedProgressBarTextLocation) -> String {
+//        if position == FlexibleSteppedProgressBarTextLocation.bottom {
+//            switch index {
+//                case 0: return "First"
+//                case 1: return "Second"
+//                case 2: return "Third"
+//                case 3: return "Fourth"
+//                case 4: return "Fifth"
+//                default: return "Date"
+//            }
+//        }
+        return ""
+    }
 }
