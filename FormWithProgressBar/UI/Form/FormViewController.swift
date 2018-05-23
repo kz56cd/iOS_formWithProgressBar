35e9c28bbde7726efb29c4a6cebdf5cbc912a0f8
kz56cd <kz56cd@gmail.com>
2018-05-23 21:26:50 +0900

    Add paging logic when back or next button tapped

--- FormWithProgressBar/UI/Form/FormViewController.swift
+++ FormWithProgressBar/UI/Form/FormViewController.swift
@@ -9,47 +9,79 @@
 import UIKit
 import Prelude
 
+enum PagingDirection {
+    case back
+    case next
+}
+
 final class FormViewController: UIViewController, ContainerViewShowing {
 
+    @IBOutlet weak var containerView: UIView!
+    
     let pageChildViewControllers: [UIViewController] = [
         StoryboardScene.FormChild01ViewController.initialScene.instantiate(),
         StoryboardScene.FormChild01ViewController.initialScene.instantiate(),
         StoryboardScene.FormChild01ViewController.initialScene.instantiate(),
         StoryboardScene.FormChild01ViewController.initialScene.instantiate()
     ]
-    
-    @IBOutlet weak var containerView: UIView!
+    let pageViewController = UIPageViewController(
+        transitionStyle: .scroll,
+        navigationOrientation: .horizontal,
+        options: nil
+    )
+    var selectedIndex = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureContainerView()
     }
     
+    // MARK: - action
     @IBAction func closeButtonTapped(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func backButtonTapped(_ sender: Any) {
+        paging(by: .reverse)
+    }
+    
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+        paging(by: .forward)
     }
 }
 
 extension FormViewController {
     fileprivate func configureContainerView() {
-        func initPageViewController() -> UIPageViewController {
-            let pageViewController = UIPageViewController(
-                transitionStyle: .scroll,
-                navigationOrientation: .horizontal,
-                options: nil
-            )
+        func preparePageViewController() {
             pageViewController.delegate = self
             pageViewController.dataSource = self
             pageViewController.setViewControllers(
                 [pageChildViewControllers.first!],
-                direction: UIPageViewControllerNavigationDirection.forward,
+                direction: .forward,
                 animated: true) { result in
                     print("page set: \(result)")
             }
-            return pageViewController
         }
         
-        addChildViewController(initPageViewController(), to: containerView)
+        preparePageViewController()
+        addChildViewController(pageViewController, to: containerView)
+    }
+    
+    fileprivate func paging(by direction: UIPageViewControllerNavigationDirection) {
+        switch direction {
+        case .reverse:
+            guard selectedIndex - 1 >= 0 else { return }
+            selectedIndex -= 1
+        case .forward:
+            guard pageChildViewControllers.count > selectedIndex + 1 else { return }
+            selectedIndex += 1
+        }
+        pageViewController.setViewControllers(
+            [pageChildViewControllers[selectedIndex]],
+            direction: direction,
+            animated: true,
+            completion: nil
+        )
     }
 }
 
@@ -62,6 +94,7 @@ extension FormViewController: UIPageViewControllerDataSource {
             index > 0 else {
                 return nil
         }
+        selectedIndex = index
         return pageChildViewControllers[index - 1]
     }
     
@@ -73,6 +106,7 @@ extension FormViewController: UIPageViewControllerDataSource {
             index < pageChildViewControllers.count - 1 else {
                 return nil
         }
+        selectedIndex = index
         return pageChildViewControllers[index + 1]
     }
 }
@@ -80,5 +114,3 @@ extension FormViewController: UIPageViewControllerDataSource {
 extension FormViewController: UIPageViewControllerDelegate {
     
 }
-
-
